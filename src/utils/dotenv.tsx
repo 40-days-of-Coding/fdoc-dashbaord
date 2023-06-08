e777bbc422685197892671effcc0bb16be8f2d5f
Aaron Will Djaba <61732071+kurtiz@users.noreply.github.com>
2023-06-08 03:04:52 +0000

    Update dotenv.tsx

--- src/utils/dotenv.tsx
+++ src/utils/dotenv.tsx
@@ -1,7 +1,7 @@
 // Parse the dotenv file content
 const dotenv: { [key: string]: string } = {
   TOKEN:
-    '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',
+    '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',
 };
 
 export default dotenv;
